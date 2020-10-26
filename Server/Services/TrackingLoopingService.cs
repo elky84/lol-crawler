55799aa264b2ac3a8d262549409e4be1717ce077
elky <elkykim@gmail.com>
2020-10-26 23:32:09 +0900

    DevelopException 발생시 로그 명확히 하기

--- Server/Services/TrackingLoopingService.cs
+++ Server/Services/TrackingLoopingService.cs
@@ -3,6 +3,7 @@ using WebUtil.Services;
 using System.Threading;
 using Serilog;
 using System;
+using Server.Exception;
 
 namespace Server.Services
 {
@@ -26,8 +27,7 @@ namespace Server.Services
                 }
                 catch (System.Exception e)
                 {
-                    Log.Logger.Error($"Implement Task Exception. Reason:{e.Message}");
-
+                    e.ExceptionLog();
                 }
 
                 await Task.Delay(TimeSpan.FromSeconds(15), stoppingToken);
