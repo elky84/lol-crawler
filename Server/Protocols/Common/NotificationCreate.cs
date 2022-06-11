2d13b1deb545a9771f6442096beedabb94b9498d
elky <elky84@gmail.com>
2022-06-11 18:00:04 +0900

    EzAspDotNet v1.0.48로 업그레이드, Notification에 컬럼 Prefix, PostFix 컬럼 추가

--- Server/Protocols/Common/NotificationCreate.cs
+++ Server/Protocols/Common/NotificationCreate.cs
@@ -1,4 +1,9 @@
-﻿namespace Server.Protocols.Common
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+
+namespace Server.Protocols.Common
 {
     public class NotificationCreate
     {
@@ -10,6 +15,21 @@
 
         public string IconUrl { get; set; }
 
+        public string Keyword { get; set; }
+
+        public string Prefix { get; set; }
+
+        public string Postfix { get; set; }
+
+        public string FilterKeyword { get; set; }
+
         public string CrawlingType { get; set; }
+
+        [JsonProperty(ItemConverterType = typeof(StringEnumConverter))]
+        public List<DayOfWeek> FilterDayOfWeeks { get; set; } = new List<DayOfWeek>();
+
+        public string FilterStartTime { get; set; }
+
+        public string FilterEndTime { get; set; }
     }
 }
